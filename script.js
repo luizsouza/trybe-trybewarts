196721ab569e08641a5fba9c729c0ff0163d5d24
Luiz <luiz.souza@gmail.com>
2022-03-02 16:46:18 -0300

    Implementando requisito 21

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ const buttonEnviar = document.querySelector('.buttonSend');
 const btnSendForm = document.querySelector('#submit-btn');
 const agree = document.querySelector('#agreement');
 const areaText = document.getElementById('textarea');
-
+const nome = document.querySelector('#input-name');
+const sobrenome = document.querySelector('#input-lastname');
+const email = document.querySelector('#input-email');
+const casa = document.querySelector('#house');
+const formulario = document.querySelector('#evaluation-form');
 
 function autenticationLogin(botao) {
   botao.preventDefault();
@@ -35,3 +39,22 @@ function contadorDeCarateresRestantes() {
 }
 
 areaText.addEventListener('input', contadorDeCarateresRestantes);
+
+function enviaFormulario() {
+  console.log('Nome: ' + nome.value);
+  console.log('Sobrenome: ' + sobrenome.value);
+  formulario.innerHTML = 
+  '<p>Nome: ' + nome.value + ' ' + sobrenome.value + '</p>' + 
+  '<p>Email: ' + email.value + '</p>' +
+  '<p>Casa: ' + casa.value + '</p>' +
+  '<p>Família: Famíla Escolhida</p>' +
+  '<p>Matérias: Matérias, Marcadas, Aqui</p>' +
+  '<p>Avaliação: NotaAqui</p>' +
+  '<p>Observações: Observações aqui</p>'
+  ;
+}
+
+btnSendForm.addEventListener('click', (e) => {
+  e.preventDefault();
+  enviaFormulario();
+});
