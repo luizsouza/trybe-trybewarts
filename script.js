fd4bf41331865d6a38af6ee49814d7b7dfe8d22e
Luiz <luiz.souza@gmail.com>
2022-03-02 17:44:33 -0300

    Todos os requisitos 100%

--- script.js
+++ script.js
@@ -41,17 +41,27 @@ function contadorDeCarateresRestantes() {
 
 areaText.addEventListener('input', contadorDeCarateresRestantes);
 
+function materiasEscolhidas() {
+  const materias = document.querySelectorAll('.subject:checked');
+  const vetorMaterias = [];
+  for (let i = 0; i < materias.length; i +=1){
+    vetorMaterias.push(materias[i].value);
+  }
+  return vetorMaterias.toString().replace(/,/g, ', ');
+}
+
 function enviaFormulario() {
   console.log('Nome: ' + nome.value);
   console.log('Sobrenome: ' + sobrenome.value);
   const familia = document.querySelector('input[name="family"]:checked').value;
   const rate = document.querySelector('input[name="rate"]:checked').value;
+  const materias = materiasEscolhidas();
   formulario.innerHTML = 
   '<p>Nome: ' + nome.value + ' ' + sobrenome.value + '</p>' + 
   '<p>Email: ' + email.value + '</p>' +
   '<p>Casa: ' + casa.value + '</p>' +
   '<p>Família: ' + familia + '</p>' +
-  '<p>Matérias: Matérias, Marcadas, Aqui</p>' +
+  '<p>Matérias: ' + materias + '</p>' +
   '<p>Avaliação: ' + rate + '</p>' +
   '<p>Observações: ' + areaText.value + '</p>'
   ;
